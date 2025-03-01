351042506dab092ee45d17ad7d50b9d0a582f050
Murathkhan <114126703+Murathkhan@users.noreply.github.com>
2025-03-01 12:10:04 +0530

    Update AuthorController.java

--- src/main/java/com/example/demo/controller/AuthorController.java
+++ src/main/java/com/example/demo/controller/AuthorController.java
@@ -3,29 +3,38 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Author;
 import com.example.demo.service.AuthorService;
 
 @RestController
+@RequestMapping("/Author")
 public class AuthorController {
 	
 	@Autowired
 	AuthorService serve;
 	
-	@GetMapping("/Author")
+	@GetMapping("/Authors")
 	public List<Author> fetchPatients() {
 		List<Author> auth = serve.fetchAll();
 		return auth;
 	}
-	@GetMapping("/Author/{id}")
+	@GetMapping("/findAuthor/{id}")
 	public List<Author> fetchAuthorByid(@PathVariable String id) {
 		List<Author> auth = serve.fetchById(id);
 		return auth;
+	}
+	
+	@PostMapping("/AddAuthor")
+	public void addAuthor(Author auth) {
+		serve.add(auth);
+	}
+	@PutMapping("/UpdateAuthor")
+	public Author updateAuthor(@RequestBody Author author) {
+		return serve.update(author);
 	}
 
 }
